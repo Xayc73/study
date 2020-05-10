3617075849f4343a8fa2537f66d79232eae4e7f5
Kirillov Sergey <kirillovsyu@gmail.com>
2020-05-10 21:43:11 +0400

    Fp. Delete many rejects, selects and condition in rating task

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,10 +5,7 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        map_array = array.reject { |film| film['rating_kinopoisk'].nil? }
-                         .reject { |film| film['rating_kinopoisk'].to_f.zero? }
-                         .reject { |film| film['country'].nil? }
-                         .select { |film| film['country'].split(/,/).length >= 2 }
+        map_array = array.select { |film| !film['rating_kinopoisk'].to_f.zero? && film['country'].to_s.split(/,/).length >= 2 }
                          .map { |film| film['rating_kinopoisk'].to_f }
         map_array.reduce(:+) / map_array.length
       end
