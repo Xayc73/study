ef907330d9a40c4376d8d21553f64a10f9f4863e
Kirillov Sergey <kirillovsyu@gmail.com>
2020-05-10 17:56:15 +0400

    Fp. Task rating done. Test asserted

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,8 +4,13 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        map_array = array.reject { |film| film['rating_kinopoisk'].nil? }
+                         .reject { |film| film['rating_kinopoisk'].to_f.zero? }
+                         .reject { |film| film['country'].nil? }
+                         .select { |film| film['country'].split(/,/).length >= 2 }
+                         .map { |film| film['rating_kinopoisk'].to_f }
+        map_array.reduce(:+) / map_array.length
       end
 
       def chars_count(_films, _threshold)
