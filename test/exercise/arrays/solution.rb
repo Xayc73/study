7fe10827af58381decbba4f9a10d1e804e40e312
Kirillov Sergey <kirillovsyu@gmail.com>
2020-05-10 18:41:03 +0400

    Array changes are not nessesary. Function map! changed to map

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -4,7 +4,7 @@ module Exercise
       def replace(array)
         max_val = array[0]
         array.each { |val| max_val = val if val > max_val }
-        array.map! { |val| val.positive? ? max_val : val }
+        array.map { |val| val.positive? ? max_val : val }
       end
 
       def search(array, query)
