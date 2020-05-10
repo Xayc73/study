fb2dada0ac4fd45123af7aec4e35f4569ccbcde9
Kirillov Sergey <kirillovsyu@gmail.com>
2020-05-10 14:23:35 +0400

    Arrays. Binary search task done. Test asserted

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -7,8 +7,24 @@ module Exercise
         array.map! { |val| val.positive? ? max_val : val }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        binary_search(array, query, 0, array.length - 1)
+      end
+
+      def binary_search(array, query, first, last)
+        return -1 if array.empty?
+
+        index = (first + (last - first) / 2).to_i
+
+        return index if array[index] == query
+
+        return -1 if first == last
+
+        if array[index] > query
+          binary_search(array, query, first, index)
+        else
+          binary_search(array, query, index + 1, last)
+        end
       end
     end
   end
