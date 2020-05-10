a299705d68e42d84a4c818764df3eb1c16ab7364
Kirillov Sergey <kirillovsyu@gmail.com>
2020-05-10 14:22:15 +0400

    Arrays. Replace task done. Test asserted

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,9 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_val = array[0]
+        array.each { |val| max_val = val if val > max_val }
+        array.map! { |val| val.positive? ? max_val : val }
       end
 
       def search(_array, _query)
